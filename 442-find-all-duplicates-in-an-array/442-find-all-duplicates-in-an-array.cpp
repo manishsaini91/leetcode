ae91a2726070dd3c2ffe6a6621fe9ec0394c15d3
Manish <manish.gla1_cs19@gla.ac.in>
2022-04-21 11:57:43 +0530

    Time: 105 ms (33.56%), Space: 44.6 MB (18.67%) - LeetHub

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
@@ -4,7 +4,7 @@ public:
         unordered_map <int,int> mp;
         vector<int> ans;
         for(int i=0;i<nums.size();i++){
-            if(mp.count(nums[i])){
+            if(mp[nums[i]]){
                 ans.push_back(nums[i]);
             }
             mp[nums[i]]=1;           
