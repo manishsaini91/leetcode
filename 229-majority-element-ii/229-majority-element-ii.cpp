ba28da6ace5bf8674bd413ed9859550d55cc796f
Manish <manish.gla1_cs19@gla.ac.in>
2022-05-01 10:33:14 +0530

    Time: 19 ms (45.02%), Space: 16 MB (12.74%) - LeetHub

---
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> v;
+        
+        if(n==1) {
+            v.push_back(nums[0]);
+            return v;
+        } 
+        unordered_map<int,int> mp;
+        for(int i=0;i<n;i++){
+            if(!mp[nums[i]])
+                mp[nums[i]]=1;
+            
+            else{
+                mp[nums[i]]++;
+               
+            }
+            
+        }
+        // for(int i=0;i<n;i++){
+             // if(mp[nums[i]]>n/3){
+                    // v.push_back(nums[i]);
+                // }
+        // }
+    for(auto i:mp){
+        if(i.second>n/3)
+            v.push_back(i.first);
+    }
+        return v;
+    }
+};
