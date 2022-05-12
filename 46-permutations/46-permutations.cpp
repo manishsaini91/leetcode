d7e040658b1d04e2843c98236b6c033d24f65029
Manish <manish.gla1_cs19@gla.ac.in>
2022-05-12 11:01:00 +0530

    Time: 5 ms (41.58%), Space: 7.7 MB (55.77%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
-    vector<vector<int>> permute(vector<int>& nums) {
-           int n=nums.size();
-        vector<int>ds;
-        vector<vector<int> > ans;
-        helper(nums,0,ans,n,ds,0);    
-        return ans;
-    }
-    int flag=INT_MAX;
-    
-    void helper(vector<int>&nums,int ind,vector<vector<int>> &ans,int n,vector<int> &ds,int start){
-        if(ds.size()==n){
-            ans.push_back(ds);
-            // ind=0;
+    void rec(int cur, vector<int> &nums, vector<vector<int>> &res) {
+        if (cur == (int)nums.size()) {
+            res.push_back(nums);
             return;
         }
-        for(int i=0;i<n;i++){
-            // vector<int> ds;
-            auto it=find(ds.begin(),ds.end(),nums[i]);
-            if(!(it!=ds.end())){
-                // flag=i;
-                ds.push_back(nums[i]);
-                helper(nums,i+1,ans,n,ds,i+1);
-                ds.pop_back();
-            }  
+        for(int i = cur; i < (int)nums.size(); i++) {
+            swap(nums[cur], nums[i]);
+            rec(cur + 1, nums, res);
+            swap(nums[cur], nums[i]);
         }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> res;
+        rec(0, nums, res);
+        return res;
     }
 };
