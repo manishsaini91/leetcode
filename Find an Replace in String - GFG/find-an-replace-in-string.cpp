55c362d861aec1145f1d4173efd5e503248a5b9c
Manish <manish.gla1_cs19@gla.ac.in>
2022-05-22 09:32:51 +0530

    Added solution - LeetHub

--- Find an Replace in String - GFG/find-an-replace-in-string.cpp
+++ Find an Replace in String - GFG/find-an-replace-in-string.cpp
@@ -10,20 +10,18 @@ class Solution {
   public:
     string findAndReplace(string S ,int Q, int index[], string sources[], string targets[]) {
         vector<pair<size_t, int>> hit;
+    for (int i = 0; i < Q; ++i)
+	    if (S.find(sources[i], index[i]) == index[i])
+		    hit.push_back({index[i], i});
 
-// check if it is a valid replacement
-for (int i = 0; i < Q; ++i)
-	if (S.find(sources[i], index[i]) == index[i])
-		hit.push_back({index[i], i});
 
-// we have to start from the end of the string to avoid messing the index.
-sort(begin(hit), end(hit), greater<>());
+        sort(begin(hit), end(hit), greater<>());
 
-// perform the replacement
-for (auto &h : hit)
-	S.replace(S.find(sources[h.second], h.first), sources[h.second].size(), targets[h.second]);
 
-return S;// code here
+    for (auto &h : hit)
+	    S.replace(S.find(sources[h.second], h.first), sources[h.second].size(), targets[h.second]);
+
+    return S;// code here
     }
 };
 
