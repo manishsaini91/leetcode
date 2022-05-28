45b1d4490a238255e4c4bca2b933fe581f04e1fd
Manish <manish.gla1_cs19@gla.ac.in>
2022-05-28 09:40:30 +0530

    Time: 605 ms (5.13%), Space: 17.9 MB (65.37%) - LeetHub

---
+++ 268-missing-number/268-missing-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        for(int i=0;i<=nums.size();i++){
+            auto it=find(nums.begin(),nums.end(),i);
+            if(it==nums.end())
+                return i;
+        }
+        return 0;
+    }
+};
