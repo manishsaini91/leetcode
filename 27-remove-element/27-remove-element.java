57777aac865bcbebb7bb81408645eb76bff2d435
Manish <manish.gla1_cs19@gla.ac.in>
2022-06-10 22:24:14 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (66.14%) - LeetHub

---
+++ 27-remove-element/27-remove-element.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int j=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==val){
+                continue;
+            }
+            else{
+                nums[j]=nums[i];
+                j++;
+                // System.out.println(nums[j]);
+            }
+        }
+        return j;
+    }
+}
