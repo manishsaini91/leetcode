ee3acbd40d77820cbd5cd7a5bfb3484b4a48818a
Manish <manish.gla1_cs19@gla.ac.in>
2022-07-04 23:04:08 -0800

    Time: 5 ms (27.38%), Space: 8.3 MB (70.30%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,28 +1,49 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-       int zero=0;
-        int one=0;
-        int two=0;
+       // int zero=0;
+       //  int one=0;
+       //  int two=0;
+       //  int n=nums.size();
+       //  for(int i=0;i<n;i++){
+       //      if(nums[i]==0){
+       //          zero++;
+       //      }
+       //      else if(nums[i]==1)
+       //          one++;
+       //      else
+       //          two++;
+       //  }
+       //  for(int i=0;i<zero;i++){
+       //      nums[i]=0;
+       //  }
+       //  for(int i=zero;i<one+zero;i++){
+       //      nums[i]=1;
+       //  }
+       //  for(int i=zero+one;i<n;i++){
+       //      nums[i]=2;
+       //  }
+       //  // return nums;
+        
+        //////two pointer approach
+        
         int n=nums.size();
-        for(int i=0;i<n;i++){
-            if(nums[i]==0){
-                zero++;
+        int low=0;
+        int high=n-1;
+        int mid=0;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums[low],nums[mid]);
+                low++;
+                mid++;
             }
-            else if(nums[i]==1)
-                one++;
-            else
-                two++;
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                swap(nums[mid],nums[high]);
+                high--;
+            }
         }
-        for(int i=0;i<zero;i++){
-            nums[i]=0;
-        }
-        for(int i=zero;i<one+zero;i++){
-            nums[i]=1;
-        }
-        for(int i=zero+one;i<n;i++){
-            nums[i]=2;
-        }
-        // return nums;
     }
 };
