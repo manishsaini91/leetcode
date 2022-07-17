8db28bdf063f9f9e258031a4e8c68a8775b54830
Manish <manish.gla1_cs19@gla.ac.in>
2022-07-17 20:25:11 +0530

    Time: 26 ms (28.99%), Space: 15.9 MB (11.87%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -19,11 +19,6 @@ public:
             }
             
         }
-        // for(int i=0;i<n;i++){
-             // if(mp[nums[i]]>n/3){
-                    // v.push_back(nums[i]);
-                // }
-        // }
     for(auto i:mp){
         if(i.second>n/3)
             v.push_back(i.first);
