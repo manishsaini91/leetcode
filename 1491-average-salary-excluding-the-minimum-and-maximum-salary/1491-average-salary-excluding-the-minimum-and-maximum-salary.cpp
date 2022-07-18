b9022f3025c79717bbc828096e1ff1f6bb0d2de9
Manish <manish.gla1_cs19@gla.ac.in>
2022-07-18 12:34:16 +0530

    Time: 3 ms (49.81%), Space: 7.2 MB (40.67%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.cpp
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.cpp
@@ -1,8 +1,18 @@
 class Solution {
 public:
     double average(vector<int>& salary) {
-        int minele=*min_element(salary.begin(),salary.end());
-        int maxele=*max_element(salary.begin(),salary.end());
+        // int minele=*min_element(salary.begin(),salary.end());
+        // int maxele=*max_element(salary.begin(),salary.end());
+        int x=salary.size();
+        int maxi=INT_MIN;
+        int mini=INT_MAX;
+        for(int i=0;i<x;i++){
+            maxi=max(maxi,salary[i]);
+        }
+        
+        for(int i=0;i<x;i++){
+            mini=min(mini,salary[i]);
+        }
         // cout<<minele<<" "<<maxele;
         double sum=0.000;
         int  count=0;
@@ -10,7 +20,7 @@ public:
            sum=sum+salary[i];
         }
         
-        sum=sum-minele-maxele;
+        sum=sum-mini-maxi;
     
         
         return sum/(salary.size()-2);
