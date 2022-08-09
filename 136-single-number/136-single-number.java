bcec7bd0c3fb8882818b5cd00fc3a1258846b9ef
Manish <manish.gla1_cs19@gla.ac.in>
2022-08-09 16:39:36 +0530

    Time: 7 ms (40.69%), Space: 50.5 MB (53.24%) - LeetHub

---
+++ 136-single-number/136-single-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        for(int i=0;i<n-1;i+=2){
+            if(nums[i] != nums[i+1])
+                return nums[i];
+        }
+        return nums[nums.length - 1];
+    }
+}
